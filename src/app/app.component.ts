c1200526a60c7dbb9801c1801d9378e713f34dac
shubhgithubrit <sg4030383@gmail.com>
2022-03-03 11:08:01 -0800

    second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'S-kit';
+ // constructor()
+ // {
+  //  setTimeout(()=>
+  //  {
+   //   this.title="3i infotech";
+   // },2000);
+  //}
+}
