22ea5a0f3cdcc2c890d9ec879cbb35b5ab4759f9
shubhgithubrit <sg4030383@gmail.com>
2022-03-11 22:23:31 -0800

    error occured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './Admin/Admin.component';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+  {
+    path:'',component:AdminComponent,children:[{path:'',component:LoginComponent}]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './Major/todos/todos.component';
 
-@NgModule({
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AdminModule } from './Admin/Admin.module';
+NgModule({
   declarations: [
     AppComponent,
     TodosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule ,
+    ReactiveFormsModule,
+    AdminModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
